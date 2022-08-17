f142470b730b068fbc74aa3b33532f7ffd4e9b4b
Matt Smiarowski <mbski@umich.edu>
2022-08-17 13:49:54 -0400

    changing relative paths for github pages, updating BG Color generator

--- bg-changer/scripts.js
+++ bg-changer/scripts.js
@@ -1,8 +1,64 @@
 const btn = document.getElementById('btn');
-btn.addEventListener('click', () => {
-  document.body.style.background = randomBg();
-});
+document.addEventListener('load', randomBg);
+btn.addEventListener('click', randomBg);
 
 function randomBg() {
-  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`
+  let h,s,l;
+  h = Math.floor(Math.random() * 360);
+  s = 100;
+  l = 50;
+  let hsl = `hsl(${h}, ${s}%, ${l}%)`;
+  let hex = hslToHex(h, s, l);
+  let rgb = hslToRgb(h, s, l);
+
+  // set DOM items
+  document.body.style.background = hsl;
+  document.getElementById('hsl').textContent = hsl;
+  document.getElementById('rgb').textContent = rgb;
+  document.getElementById('hex').textContent = hex;
+}
+
+
+function hslToHex(h, s, l) {
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = n => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+
+function hslToRgb(h,s,l) {
+  // Must be fractions of 1
+  s /= 100;
+  l /= 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+      m = l - c/2,
+      r = 0,
+      g = 0,
+      b = 0;
+
+  if (0 <= h && h < 60) {
+    r = c; g = x; b = 0;  
+  } else if (60 <= h && h < 120) {
+    r = x; g = c; b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0; g = c; b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0; g = x; b = c;
+  } else if (240 <= h && h < 300) {
+    r = x; g = 0; b = c;
+  } else if (300 <= h && h < 360) {
+    r = c; g = 0; b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
 }
