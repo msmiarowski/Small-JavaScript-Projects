d7614cc8445a70e7e1fcb8a3feac25413509a3eb
Matt Smiarowski <mbski@umich.edu>
2022-10-28 15:32:19 -0400

    small changes

--- follow-mouse/scripts.js
+++ follow-mouse/scripts.js
@@ -1,16 +1,3 @@
-// document.addEventListener('mousemove', e => {
-//   let body = document.querySelector('body');
-//   let icon = document.querySelector('#circle');
-//   // console.log(e.clientX, e.clientY);
-//   // let left = e.pageX;
-//   // let top = e.pageY;
-//   // icon.style.left = `${left}px`;
-//   // icon.style.top = `${top}px`;
-//   icon.style.setProperty('--xPos', e.clientX+'px');
-//   icon.style.setProperty('--yPos', e.clientY+'px');
-//   icon.style.opacity = 1;
-// });
-
 let icon =  document.querySelector('#icon');
 let embedContainer = document.querySelector('.embed-container');
 let isFiring = false;
@@ -25,28 +12,10 @@ let yPos = 0;
 
 let timeElapsed = 0;
 let visibility = true;
-    
-
-// let ghost = document.querySelector("#ghost");
 	  
 let headerFloatie = document.querySelector(".floatie");
 // let headerFloatieSVG = document.querySelector(".floatie svg");
 let permanentlyHideFloatie = false;
-
-// let isFiring = false;
-
-// let mouseX = 0;
-// let mouseY = 0;
-
-// let dX = 0;
-// let dY = 0;
-
-// let xPos = 0;
-// let yPos = 0;
-  
-// let ghostTimeElapsed = 0;
-// let ghostVisibility = true;
-
 
 function throttleEvent(event) {
   if (isFiring === false) {
@@ -84,7 +53,7 @@ function setInitialIconPosition() {
 
 setInitialIconPosition();
 
-  function setIconPosition(event) {
+function setIconPosition(event) {
   if (permanentlyHideFloatie == false) {
     timeElapsed = 0;
     visibility = true;
@@ -92,18 +61,23 @@ setInitialIconPosition();
   } else {
     hideFloatie();
   }
-  
-    mouseX = event.clientX;
-    mouseY = event.clientY;
-  }
+
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+}
 
 function moveIcon() {
   if (permanentlyHideFloatie == false) {
+    console.log(mouseX, xPos, mouseY, yPos)
     dX = mouseX - xPos;
     dY = mouseY - yPos;
 
+    console.log(dX, dY);
+
     xPos += (dX / 100);
     yPos += (dY / 100);
+
+    console.log(xPos, yPos)
 
     icon.style.setProperty("--xPos", xPos + "px");
     icon.style.setProperty("--yPos", yPos + "px");
@@ -116,7 +90,7 @@ function moveIcon() {
 
     timeElapsed++;
 
-
+    console.log(timeElapsed);
     if ((timeElapsed > 300) && (visibility == true)) {
       hideFloatie();
       visibility = false;
